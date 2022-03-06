d6d38b4f2b1e48b3704e62e85ee6c7fa94327a83
LanMeiX <2099289392@qq.com>
2022-03-06 19:11:34 +0800

    初次提交的版本

---
+++ src/main/java/com/lemon/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lemon.git;
+
+/**
+ * @Author Lemons
+ * @create 2022-03-06-18:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

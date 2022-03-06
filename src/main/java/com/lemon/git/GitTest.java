f569a149a03cb732e0aea93ee2716b1783139105
LanMeiX <2099289392@qq.com>
2022-03-06 20:09:26 +0800

    master分支上第最后一次提交的版本

--- src/main/java/com/lemon/git/GitTest.java
+++ src/main/java/com/lemon/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello java");
         System.out.println("hello, python");
+        System.out.println("hello c++");
     }
 }
